0970e33d49e5809cfb22eb802b2f77c8feeecf3e
ErikDale <erikdale@stud.ntnu.no>
2022-11-07 11:08:53 +0100

    Made file with methods that extracts data stock prices/tweets from the dataset.

---
+++ prepare_data.py
@@ -0,0 +1,148 @@
+import numpy as np
+import os
+import json
+import re
+
+
+def combine_tweets_prices():
+    """
+    Method that combines the tweets and prices that fall on the same day
+    """
+    price_values, targets = extract_prices()
+
+    tweets = extract_tweets()
+
+    keys = tweets.keys()
+
+    combined = []
+
+    for i in range(len(price_values)):
+        for key in keys:
+            if key == price_values[i][0][0]:
+                keys_str = [str(j) for j in tweets[key]]
+                values_str = [str(k) for k in price_values[i][0]]
+                combined.append(keys_str + values_str)
+                break
+
+    [r.pop(6) for r in combined]
+
+    target_combined = combined[5:]
+
+    targets = []
+
+    for i in range(len(target_combined)):
+        if float(target_combined[i][6]) > float(target_combined[i][9]):
+            targets.append(0)
+        else:
+            targets.append(1)
+
+    values = []
+
+    for i in range(len(combined)):
+        if i > len(combined) - 5:
+            break
+        value = []
+        for j in range(i, i + 5):
+            value.append(combined[j])
+
+        values.append(value)
+
+    values = values[:len(targets)]
+
+    for i in range(len(values)):
+        for j in range(len(values[i])):
+            for k in range(len(values[i][j])):
+                values[i][j][k] = float(values[i][j][k])
+    return values, targets
+
+
+def make_text_into_numbers(text, uniquewords):
+    """
+    Method that converts text to a list of numbers
+    """
+    iwords = text.lower().split(' ')
+    numbers = []
+    for n in iwords:
+        try:
+            numbers.append(uniquewords.index(n))
+        except ValueError:
+            numbers.append(0)
+    numbers = numbers + [0,0,0,0,0]
+
+    return numbers[:6]
+
+
+def read_text_file(file_path, dict, file_name):
+    """
+    Method that reads the json object in the tweet file and puts them into a dictionary
+    """
+    array = []
+    with open(file_path, 'r') as f:
+        for jsonObj in f:
+            if len(jsonObj) != 0:
+                data = json.loads(jsonObj)
+                # Reading only the text from the tweets excluding date and userid
+                text = data["text"]
+                text = ' '.join(text)
+                array.append(text)
+        dict[file_name] = ' '.join(array)
+
+
+def extract_tweets():
+    """
+    Method that extracts the tweets
+    :return: an array containing the different tweets
+    """
+    dict = {}
+    # Folder Path
+    path = "./stocknet-dataset-master/tweet/preprocessed/AAPL"
+    # Read text File
+    # iterate through all file
+    for file in os.listdir(path):
+        file_path = f"{path}/{file}"
+        read_text_file(file_path, dict, file)
+    # Removing non-alphanumeric character from the tweets
+    for key in dict.keys():
+        string = dict[key]
+        string = re.sub(r'[^A-Za-z0-9 ]+', '', string)
+        dict[key] = string
+
+    allwords = ' '.join(dict.values()).lower().split(' ')
+    uniquewords = list(set(allwords))
+    for key in dict.keys():
+        string = dict[key]
+        vector = make_text_into_numbers(string, uniquewords)
+        dict[key] = vector
+    return dict
+
+
+def extract_prices():
+    """Method that extracts the prices of the stocks"""
+    cols = [0, 1, 2, 3, 4, 5]
+
+    aapl = np.loadtxt("./stocknet-dataset-master/price/raw/AAPL.csv", delimiter=",", dtype=str, usecols=cols,
+                      skiprows=1)
+
+    target_prices = aapl[5:]
+
+    targets = []
+
+    for i in range(len(target_prices)):
+        if float(target_prices[i][1]) > float(target_prices[i][4]):
+            targets.append(0)
+        else:
+            targets.append(1)
+
+    values = []
+
+    for i in range(len(aapl)):
+        if i > len(aapl) - 5:
+            break
+        value = []
+        for j in range(i, i + 5):
+            value.append(aapl[j])
+
+        values.append(value)
+
+    values = values[:len(targets)]
+    return values, targets
